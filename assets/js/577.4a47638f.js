c4b0a83559ab96a6bc2686e726e369a14a4cd64f
CristianLara <xcristianlara@gmail.com>
2024-10-03 21:17:01 +0000

    Deploying to gh-pages from  @ 5b8fa3be480bef9134b4cc4763765913f78dad72 🚀

--- assets/js/577.4a47638f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk=self.webpackChunk||[]).push([[577],{549:(s,e,c)=>{"use strict";c.d(e,{A:()=>u});var k=c(8291);const u=k},5741:()=>{}}]);
---
+++ assets/js/b955862e.318d16ac.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[291],{3998:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/Ax/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":2,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
